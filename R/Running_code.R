a46ebde5ba886b3971dc1ee4fbf89306857cd7b8
Marcelo Ponce <mponce@scinet.utoronto.ca>
2019-05-01 00:29:10 -0400

    modifications to example script so that incorporates the new loader fn and clarifying section where 'mock' data is generated

--- R/Running_code.R
+++ R/Running_code.R
@@ -1,21 +1,10 @@
 # **Should have all the function R files and MPLN.stan in the same working directory as this file
 
-# Reading needed code
-
-source("Calc_likelihood.R")
-source("Calculate_parameters.R")
-source("Calling_clustering.R")
-source("Cluster_mpln.R")
-source("Initialization_run.R")
-source("Main_mpln.R")
-source("Model_selection.R")
-source("MPLNdata_generator.R")
-source("Remove_Zero_Counts.R")
-source("Stan_run.R")
-source("Visualize_mpln.R")
-source("Zvalue_calculation.R")
+# Loading MPLNClust fns
+source("_loader.R")
 
 
+#####################################  DATA GENERATION/LOADING  #####################################
 # Values for data simulation
 true_mu1 <- c(6.5,6,6,6,6,6)  
 true_mu2 <- c(2,2.5,2,2,2,2) 
@@ -25,9 +14,8 @@ true_sigma2 <- diag(6)
 
 # Data simulated is saved as 'simulated_counts'
 simulated_counts <- Datagenerator_mpln(N = 50, d = 6, pi_g = c(0.79,0.21), means = rbind(true_mu1,true_mu2), sigmas = rbind(true_sigma1,true_sigma2), ProduceImage="Yes")
+#####################################################################################################
 
-# Checking/Loading needed packages
-LoadCheckPkg(pckgs=c("parallel","rstan","Rcpp","mclust","mvtnorm","edgeR","capushe","clusterGeneration","coda"))
 
 # Making RStan model 
 mod = stan_model("MPLN.stan")
