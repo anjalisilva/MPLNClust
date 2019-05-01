71b40f7909e4aa9d9a59b2d565d0193c48b2e556
Marcelo Ponce <mponce@scinet.utoronto.ca>
2019-05-01 17:03:21 -0400

    adding an optinal argument to the Remove_Zero_Counts fn, so that the list of removed elements can be saved in a file for inspection if desired

--- R/Remove_Zero_Counts.R
+++ R/Remove_Zero_Counts.R
@@ -1,14 +1,20 @@
 # Calculate and remove rows with zeros
-remove_zero_counts<-function(dataset){
+remove_zero_counts<-function(dataset,output=FALSE){
 
   zeroSUMrows <- which(rowSums(dataset)==0)
   nbrZeroSUMrows <- length(zeroSUMrows)
   
+
+  if (output) {
+    write.csv(rownames(dataset[zeroSUMrows,]), file=paste0("zeroSUMrows_",format(Sys.time(), "%d%b%Y"),".csv") )
+  }
+
   if (nbrZeroSUMrows > 0) {
       dataset <- dataset[-zeroSUMrows,]
       cat(paste(nbrZeroSUMrows,"row(s) removed from the dataset because sum=0",'\n'))
   }
-  
+
+ 
   RESULTS <- list(dataset= dataset)
   class(RESULTS) <- "MPLN_ZerosRemoved"
   return(RESULTS)
