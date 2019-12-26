71429a5f22ac6b85cb3d6a67eadc48687587c549
anjalisilva <anjali@uoguelph.ca>
2019-12-26 16:45:11 -0500

    Updated calcLikelihood.R

--- R/calcLikelihood.R
+++ R/calcLikelihood.R
@@ -2,19 +2,22 @@
 calcLikelihood <- function(z, PI, dataset, 
                            mu_g, G, Sig_g, 
                            thetaStan, normFactors) { 
-  n <- nrow(dataset)
-  like <- matrix(NA, nrow = n, ncol = G)
-  for (g in 1:G) {
-    for (i in 1:n) {
+  nObservations <- nrow(dataset)
+  like <- matrix(NA, nrow = nObservations, ncol = G)
+  for (g in seq_along(1:G)) {
+    for (i in seq_along(1:nObservations)) {
       x <- thetaStan[[g]][i, ]
-      d <- ncol(dataset)
-      like[i, g] <- (z[i,g] * (log(PI[g]) +
+      dimensionality <- ncol(dataset)
+      like[i, g] <- (z[i, g] * (log(PI[g]) +
                     t(dataset[i, ]) %*% (x + normFactors) - 
                     sum(exp(x + normFactors)) - 
-                    sum(lfactorial(dataset[i, ])) - d / 2 * log(2 * pi) - 
-                    1 / 2 * log(det(Sig_g[((g - 1) * d + 1):(g * d), ])) - 
+                    sum(lfactorial(dataset[i, ])) - 
+                    dimensionality / 2 * log(2 * pi) - 
+                    1 / 2 * log(det(Sig_g[((g - 1) * 
+                    dimensionality + 1):(g * dimensionality), ])) - 
                     0.5 * t(x-mu_g[g, ]) %*% 
-                    solve(Sig_g[((g - 1) * d + 1):(g * d), ]) %*% 
+                    solve(Sig_g[((g - 1) * 
+                    dimensionality + 1):(g * dimensionality), ]) %*% 
                     (x - mu_g[g, ])))
     }
   }
