10e460428b858e21df219f22b1cdbdceb4b064f3
anjalisilva <anjali@uoguelph.ca>
2020-01-12 20:20:40 -0500

    Updated testing for data generation

--- tests/testthat/test-mplnDataGenerator.R
+++ tests/testthat/test-mplnDataGenerator.R
@@ -1,8 +1,9 @@
-test_that("data generation", {
-  library(MPLNClust)
+context("Checking for data simulation")
+library(MPLNClust)
+
+test_that("Data generation is as expected", {
 
   # Generating simulated data
-
   trueMu1 <- c(6.5, 6, 6, 6, 6, 6)
   trueMu2 <- c(2, 2.5, 2, 2, 2, 2)
 
@@ -11,14 +12,29 @@ test_that("data generation", {
 
   set.seed(1234)
   simulated_counts <- mplnDataGenerator(nObservations = 50,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.21),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "No")
+                                        dimensionality = 6,
+                                        mixingProportions = c(0.79, 0.21),
+                                        mu = rbind(trueMu1, trueMu2),
+                                        sigma = rbind(trueSigma1, trueSigma2),
+                                        produceImage = "No")
 
   expect_that(length(simulated_counts), equals(9))
   expect_that(simulated_counts, is_a("mplnDataGenerator"))
   expect_that(trunc(simulated_counts$observations), equals(50))
   expect_that(trunc(simulated_counts$dimensionality), equals(6))
 })
+
+
+context("Checking for invalid user input")
+test_that("Data generation error upon invalid user input", {
+
+  set.seed(1234)
+  simulated_counts <- mplnDataGenerator(nObservations = "50",
+    dimensionality = 6,
+    mixingProportions = c(0.79, 0.21),
+    mu = rbind(trueMu1, trueMu2),
+    sigma = rbind(trueSigma1, trueSigma2),
+    produceImage = "No")
+
+  expect_error(importBED(pathToInvalidFile1))
+}
