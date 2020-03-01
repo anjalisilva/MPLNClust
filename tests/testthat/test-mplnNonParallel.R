916a5a68d7c4021aabf868341784cbb649f1a66e
Anjali Silva <anjali.silva@uhnresearch.ca>
2020-03-01 00:40:56 -0500

    Updated tests to be shorter

--- tests/testthat/test-mplnNonParallel.R
+++ tests/testthat/test-mplnNonParallel.R
@@ -21,7 +21,7 @@ test_that("Checking clustering results", {
   mplnNonParallelResults <- MPLNClust::mplnNonParallel(
                                 dataset = simulated_counts$dataset,
                                 membership = simulated_counts$trueMembership,
-                                gmin = 1,
+                                gmin = 2,
                                 gmax = 2,
                                 nChains = 3,
                                 nIterations = 500,
@@ -32,10 +32,7 @@ test_that("Checking clustering results", {
   expect_that(length(mplnNonParallelResults), equals(16))
   expect_that(mplnNonParallelResults, is_a("mplnNonParallel"))
   expect_that(mplnNonParallelResults$initalization_method, equals("kmeans"))
-  normFactors <- c(-0.62996925, -0.15999746,  0.12852236,
-    0.34015676,  0.28591670,  0.03537089)
-  expect_that(mplnNonParallelResults$normalization_factors, equals(normFactors))
-  numPara <- c(27, 55)
+  numPara <- c(27)
   expect_that(mplnNonParallelResults$numb_of_parameters, equals(numPara))
   expect_that(mplnNonParallelResults$true_labels, equals(simulated_counts$trueMembership))
   expect_that(trunc(mplnNonParallelResults$ICL_all$ICLmodelselected), equals(2))
@@ -55,145 +52,145 @@ test_that("Data clustering error upon invalid user input", {
 
   set.seed(1234)
   simulated_counts <- mplnDataGenerator(nObservations = 500,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.21),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "No")
+                                        dimensionality = 6,
+                                        mixingProportions = c(0.79, 0.21),
+                                        mu = rbind(trueMu1, trueMu2),
+                                        sigma = rbind(trueSigma1, trueSigma2),
+                                        produceImage = "No")
 
   # dataset provided as character
   expect_error(mplnNonParallel(dataset = "dataset",
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # dataset provided as logical
   expect_error(mplnNonParallel(dataset = NA,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect size for true membership
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership[-1],
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership[-1],
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect class for true membership
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = "trueMembership",
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = "trueMembership",
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect input type for gmin
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = "1",
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = "1",
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect input for gmin and gmax
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 5,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 5,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
 
   # Incorrect input type for nChains
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = "3",
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = "3",
+                               nIterations = 1000,
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect input type for nIterations
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = "1000",
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = "1000",
+                               initMethod = "kmeans",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect input type for initMethod
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "other",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = "other",
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
 
   # Incorrect input type for initMethod
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = NA,
-    nInitIterations = 3,
-    normalize = "Yes"))
+                               membership = simulated_counts$trueMembership,
+                               gmin = 1,
+                               gmax = 2,
+                               nChains = 3,
+                               nIterations = 1000,
+                               initMethod = NA,
+                               nInitIterations = 3,
+                               normalize = "Yes"))
 
   # Incorrect input type for nInitIterations
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = "3",
-    normalize = "Yes"))
+                                membership = simulated_counts$trueMembership,
+                                gmin = 1,
+                                gmax = 2,
+                                nChains = 3,
+                                nIterations = 1000,
+                                initMethod = "kmeans",
+                                nInitIterations = "3",
+                                normalize = "Yes"))
 
   # Incorrect input type for normalize
   expect_error(mplnNonParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Other"))
+                                membership = simulated_counts$trueMembership,
+                                gmin = 1,
+                                gmax = 2,
+                                nChains = 3,
+                                nIterations = 1000,
+                                initMethod = "kmeans",
+                                nInitIterations = 3,
+                                normalize = "Other"))
 
 })
 
--- tests/testthat/test-mplnParallel.R
+++ tests/testthat/test-mplnParallel.R
@@ -11,23 +11,23 @@ test_that("Checking clustering results", {
   trueSigma2 <- diag(6)
 
   set.seed(1234)
-  simulated_counts <- mplnDataGenerator(nObservations = 100,
+  simulatedCounts <- mplnDataGenerator(nObservations = 50,
                                         dimensionality = 6,
                                         mixingProportions = c(0.79, 0.21),
                                         mu = rbind(trueMu1, trueMu2),
                                         sigma = rbind(trueSigma1, trueSigma2),
                                         produceImage = "No")
   set.seed(1234)
-  mplnResults <- MPLNClust::mplnParallel(dataset = simulated_counts$dataset,
-                                membership = simulated_counts$trueMembership,
-                                gmin = 1,
-                                gmax = 2,
-                                nChains = 3,
-                                nIterations = 800,
-                                initMethod = "kmeans",
-                                nInitIterations = 1,
-                                normalize = "Yes",
-                                numNodes = 2)
+  mplnResults <- MPLNClust::mplnParallel(dataset = simulatedCounts$dataset,
+                                         membership = simulatedCounts$trueMembership,
+                                         gmin = 2,
+                                         gmax = 2,
+                                         nChains = 3,
+                                         nIterations = 300,
+                                         initMethod = "kmeans",
+                                         nInitIterations = 1,
+                                         normalize = "Yes",
+                                         numNodes = 2)
 
   # Setting numNodes = 2 based on the following entry, otherwise error.
   # "NB: you can’t use unexported functions and you shouldn’t open new graphics
@@ -38,12 +38,9 @@ test_that("Checking clustering results", {
   expect_that(length(mplnResults), equals(16))
   expect_that(mplnResults, is_a("mplnParallel"))
   expect_that(mplnResults$initalization_method, equals("kmeans"))
-  normFactors <- c( -0.20985978,  0.07542025, -0.16272084,
-    0.43821243, -0.21190868,  0.07085663)
-  expect_that(mplnResults$normalization_factors, equals(normFactors))
-  numPara <- c(27, 55)
+  numPara <- c(27)
   expect_that(mplnResults$numb_of_parameters, equals(numPara))
-  expect_that(mplnResults$true_labels, equals(simulated_counts$trueMembership))
+  expect_that(mplnResults$true_labels, equals(simulatedCounts$trueMembership))
   expect_that(trunc(mplnResults$ICL_all$ICLmodelselected), equals(2))
   expect_that(trunc(mplnResults$AIC_all$AICmodelselected), equals(2))
   expect_that(trunc(mplnResults$BIC_all$BICmodelselected), equals(2))
@@ -60,146 +57,146 @@ test_that("Data clustering error upon invalid user input", {
   trueSigma2 <- diag(6)
 
   set.seed(1234)
-  simulated_counts <- mplnDataGenerator(nObservations = 500,
-    dimensionality = 6,
-    mixingProportions = c(0.79, 0.21),
-    mu = rbind(trueMu1, trueMu2),
-    sigma = rbind(trueSigma1, trueSigma2),
-    produceImage = "No")
+  simulatedCounts <- mplnDataGenerator(nObservations = 500,
+                                        dimensionality = 6,
+                                        mixingProportions = c(0.79, 0.21),
+                                        mu = rbind(trueMu1, trueMu2),
+                                        sigma = rbind(trueSigma1, trueSigma2),
+                                        produceImage = "No")
 
   # dataset provided as character
   expect_error(mplnParallel(dataset = "dataset",
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # dataset provided as logical
   expect_error(mplnParallel(dataset = NA,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect size for true membership
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership[-1],
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership[-1],
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect class for true membership
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = "trueMembership",
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = "trueMembership",
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect input type for gmin
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = "1",
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = "1",
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect input for gmin and gmax
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 5,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 5,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
 
   # Incorrect input type for nChains
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = "3",
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = "3",
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect input type for nIterations
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = "1000",
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = "1000",
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect input type for initMethod
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "other",
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "other",
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
 
   # Incorrect input type for initMethod
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = NA,
-    nInitIterations = 3,
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = NA,
+                            nInitIterations = 3,
+                            normalize = "Yes"))
 
   # Incorrect input type for nInitIterations
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = "3",
-    normalize = "Yes"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = "3",
+                            normalize = "Yes"))
 
   # Incorrect input type for normalize
-  expect_error(mplnParallel(dataset = simulated_counts$dataset,
-    membership = simulated_counts$trueMembership,
-    gmin = 1,
-    gmax = 2,
-    nChains = 3,
-    nIterations = 1000,
-    initMethod = "kmeans",
-    nInitIterations = 3,
-    normalize = "Other"))
+  expect_error(mplnParallel(dataset = simulatedCounts$dataset,
+                            membership = simulatedCounts$trueMembership,
+                            gmin = 1,
+                            gmax = 2,
+                            nChains = 3,
+                            nIterations = 1000,
+                            initMethod = "kmeans",
+                            nInitIterations = 3,
+                            normalize = "Other"))
 
 })
 
