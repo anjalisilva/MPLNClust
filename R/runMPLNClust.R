c3ee3c86bd265b048a8e2ebaf5d130bc63178ba4
Anjali Silva <anjali.silva@uhnresearch.ca>
2020-04-27 13:58:05 -0400

    Added shiny for testing

---
+++ R/runMPLNClust.R
@@ -0,0 +1,21 @@
+#' Launch the shiny app for package MPLNClust
+#'
+#' A function that launches the shiny app for this package.
+#' The code has been placed in \code{./inst/shiny-scripts}.
+#'
+#' @return No return value but open up a shiny page.
+#'
+#' @examples
+#' \dontrun{
+#' runMPLNClust()
+#' }
+#'
+#' @export
+#' @importFrom shiny runApp
+
+runMPLNClust <- function() {
+  #appDir <- system.file("shiny-scripts",
+  #                      package = "MPLNClust")
+  #shiny::runApp(appDir, display.mode = "normal")
+  #return()
+}
