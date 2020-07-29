f59f49bb006a0ef9825c7f983c71e7a5e642f824
anjalisilva <anjali@uoguelph.ca>
2020-07-29 10:28:24 -0400

    Minor modification to example

--- R/mplnVariationalClustering.R
+++ R/mplnVariationalClustering.R
@@ -67,7 +67,7 @@
 #' # trueSigma1 <- diag(6) * 2
 #' # trueSigma2 <- diag(6)
 #'
-#' # sampleData <- mplnDataGenerator(nObservations = 100,
+#' # sampleData <- mplnDataGenerator(nObservations = 1000,
 #' #                                 dimensionality = 6,
 #' #                                 mixingProportions = c(0.79, 0.21),
 #' #                                 mu = rbind(trueMu1, trueMu2),
